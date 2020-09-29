e79049c9eeb088f2c6b6f804d767d3b85d3242ab
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2020-09-29 19:24:03 +0200

    Shadow added

--- TFG/Assets/Scripts/Movement.cs
+++ TFG/Assets/Scripts/Movement.cs
@@ -16,12 +16,14 @@ public class Movement : MonoBehaviour
     SpriteRenderer engineer_sprite;
     Player_States player_state = Player_States.IDLE;
     Animator anim;
+    Transform shadow;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = transform.GetChild(0).GetComponent<Animator>();
         engineer_sprite = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        shadow = transform.GetChild(0).GetChild(0);
     }
 
     // Update is called once per frame
@@ -61,10 +63,12 @@ public class Movement : MonoBehaviour
         if (direction != Vector2.zero && player_state!=Player_States.RUN)
         {
             player_state = Player_States.RUN;
+            shadow.localScale = new Vector3(3, 1, 0.8f);
         }
         else if (direction == Vector2.zero && player_state != Player_States.IDLE)
         {
             player_state = Player_States.IDLE;
+            shadow.localScale = new Vector3(2.75f, 1, 0.8f);
         }
 
         anim.SetInteger("State", (int)player_state);
