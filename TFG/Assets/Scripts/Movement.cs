201ae052eb5bf1f14c46423e06604b8a96d81b49
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2020-09-29 18:59:44 +0200

    Player Added

--- TFG/Assets/Scripts/Movement.cs
+++ TFG/Assets/Scripts/Movement.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    enum Player_States
+    {
+        IDLE,
+        RUN
+    }
+
     Rigidbody2D rb;
     Vector2 direction = new Vector2();
     public float speed = 3;
+    SpriteRenderer engineer_sprite;
+    Player_States player_state = Player_States.IDLE;
+    Animator anim;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = transform.GetChild(0).GetComponent<Animator>();
+        engineer_sprite = transform.GetChild(0).GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -38,6 +49,25 @@ public class Movement : MonoBehaviour
             direction.x += speed;
         }
 
+        if (direction.x < 0 && !engineer_sprite.flipX)
+        {
+            engineer_sprite.flipX = true;
+        }
+        else if (direction.x > 0 && engineer_sprite.flipX)
+        {
+            engineer_sprite.flipX = false;
+        }
+
+        if (direction != Vector2.zero && player_state!=Player_States.RUN)
+        {
+            player_state = Player_States.RUN;
+        }
+        else if (direction == Vector2.zero && player_state != Player_States.IDLE)
+        {
+            player_state = Player_States.IDLE;
+        }
+
+        anim.SetInteger("State", (int)player_state);
         rb.velocity = direction;
     }
 }
