90af17b78dbbfedb961b9c8084a31ee82e8eba1a
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2020-09-30 14:28:41 +0200

    Box Logic almost finished

--- TFG/Assets/Scripts/BoxBehaviour.cs
+++ TFG/Assets/Scripts/BoxBehaviour.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.U2D;
 
 public class BoxBehaviour : MonoBehaviour
 {
+    // Components
+    Animator anim;
+
+    // Inspector Variables
+    public bool is_static = false;
+    public float time_to_reset = 1.5f;
+
+    // Internal Variables
     enum TRIGGER_DIRECTION
     {
         IDLE,
@@ -14,11 +23,10 @@ public class BoxBehaviour : MonoBehaviour
         EAST
     }
 
-    Animator anim;
     TRIGGER_DIRECTION trigger_dir = TRIGGER_DIRECTION.IDLE;
     bool can_move = true;
 
-    public float time_to_reset = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -32,32 +40,110 @@ public class BoxBehaviour : MonoBehaviour
 
     public void Triggered(GameObject GO)
     {
-        if (trigger_dir == TRIGGER_DIRECTION.IDLE && can_move)
+        if (!is_static && trigger_dir == TRIGGER_DIRECTION.IDLE && can_move)
         {
+            Vector2 direction = new Vector2();
+            Vector2 offset = new Vector2();
+
             if (GO.name == "North")
             {
                 trigger_dir = TRIGGER_DIRECTION.NORTH;
+                direction = new Vector2(0, -1);
+                offset = new Vector2(0, -1);
             }
             else if (GO.name == "South")
             {
                 trigger_dir = TRIGGER_DIRECTION.SOUTH;
+                direction = new Vector2(0, 1);
+                offset = new Vector2(0, 1);
             }
             else if (GO.name == "West")
             {
                 trigger_dir = TRIGGER_DIRECTION.WEST;
+                direction = new Vector2(1, 0);
+                offset = new Vector2(1, 0);
             }
             else if (GO.name == "East")
             {
                 trigger_dir = TRIGGER_DIRECTION.EAST;
+                direction = new Vector2(-1, 0);
+                offset = new Vector2(-1, 0);
             }
 
+            RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x + offset.x, transform.position.y + offset.y), direction);
+
+            if (hit.collider != null && !hit.collider.isTrigger)
+            {
+                switch (trigger_dir)
+                {
+                    case TRIGGER_DIRECTION.NORTH:
+                        {
+                            if (Mathf.Abs((transform.position.y - hit.transform.position.y)) <= 1.5f)
+                            {
+                                trigger_dir = TRIGGER_DIRECTION.IDLE;
+                                Debug.Log("VA A CHOCAR!!!");
+                                return;
+                            }
+                            break;
+                        }
+                    case TRIGGER_DIRECTION.SOUTH:
+                        {
+                            if (Mathf.Abs((transform.position.y - hit.transform.position.y)) <= 1.5f)
+                            {
+                                trigger_dir = TRIGGER_DIRECTION.IDLE;
+                                Debug.Log("VA A CHOCAR!!!");
+                                return;
+                            }
+                            break;
+                        }
+                    case TRIGGER_DIRECTION.WEST:
+                        {
+                            if (Mathf.Abs((transform.position.x - hit.transform.position.x)) <= 1.5f)
+                            {
+                                trigger_dir = TRIGGER_DIRECTION.IDLE;
+                                Debug.Log("VA A CHOCAR!!!");
+                                return;
+                            }
+                            break;
+                        }
+                    case TRIGGER_DIRECTION.EAST:
+                        {
+                            if (Mathf.Abs((transform.position.x - hit.transform.position.x)) <= 1.5f)
+                            {
+                                trigger_dir = TRIGGER_DIRECTION.IDLE;
+                                Debug.Log("VA A CHOCAR!!!");
+                                return;
+                            }
+                            break;
+                        }
+                }
+            }
+
+            GO.transform.GetChild(0).gameObject.SetActive(true);
             can_move = false;
             anim.SetInteger("Direction", (int)trigger_dir);
+
         }
     }
 
     void Stop()
     {
+        switch(trigger_dir)
+        {
+            case TRIGGER_DIRECTION.NORTH:
+                transform.GetChild(2).GetChild(0).gameObject.SetActive(false);
+                break;
+            case TRIGGER_DIRECTION.EAST:
+                transform.GetChild(5).GetChild(0).gameObject.SetActive(false);
+                break;
+            case TRIGGER_DIRECTION.WEST:
+                transform.GetChild(4).GetChild(0).gameObject.SetActive(false);
+                break;
+            case TRIGGER_DIRECTION.SOUTH:
+                transform.GetChild(3).GetChild(0).gameObject.SetActive(false);
+                break;
+        }
+
         trigger_dir = TRIGGER_DIRECTION.IDLE;
         anim.SetInteger("Direction", (int)trigger_dir);
         anim.SetTrigger("Stopped");
--- TFG/Assets/Scripts/Box_Trigger.cs
+++ TFG/Assets/Scripts/Box_Trigger.cs
@@ -11,4 +11,12 @@ public class Box_Trigger : MonoBehaviour
             transform.parent.GetComponent<BoxBehaviour>().Triggered(gameObject);
         }
     }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            transform.parent.GetComponent<BoxBehaviour>().Triggered(gameObject);
+        }
+    }
 }
